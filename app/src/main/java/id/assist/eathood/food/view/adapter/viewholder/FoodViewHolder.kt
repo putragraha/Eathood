7d6795bec8b1a75db0ee46344198cceb208693fc
Putra <putra.nugraha@dana.id>
2019-09-20 05:24:35 +0700

    Create single responsibility view holder

---
+++ app/src/main/java/id/assist/eathood/food/view/adapter/viewholder/FoodViewHolder.kt
@@ -0,0 +1,26 @@
+package id.assist.eathood.food.view.adapter.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import id.assist.eathood.food.model.Food
+import kotlinx.android.synthetic.main.view_food_item.view.*
+
+
+/**
+ * @author Putra Nugraha (putra.nugraha@dana.id)
+ * @version FoodViewHolder, v 1.3.0 2019-09-20 05:20 by Putra Nugraha
+ */
+
+
+class FoodViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(food: Food, listener: (Food) -> Unit) = with(itemView) {
+        iv_meal.setImageBitmap(food.picture)
+        tv_food_name.text = food.name
+        tv_description.text = food.description
+        setOnClickListener {
+            listener(food)
+        }
+    }
+
+}
