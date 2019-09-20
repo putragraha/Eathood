7a6ce39aa6d8acd18660222b8ac94546eb878e11
Putra <putra.nugraha@dana.id>
2019-09-20 09:02:47 +0700

    Add liskov substitution and dependency inversion

---
+++ app/src/main/java/id/assist/eathood/contract/TextViewDecorator.kt
@@ -0,0 +1,13 @@
+package id.assist.eathood.contract
+
+import android.widget.TextView
+
+
+/**
+ * @author Putra Nugraha (putra.nugraha@dana.id)
+ * @version TextViewDecorator, v 1.3.0 2019-09-20 08:39 by Putra Nugraha
+ */
+interface TextViewDecorator {
+
+    fun doMagic(textView: TextView)
+}
---
+++ app/src/main/java/id/assist/eathood/food/textviewdecorator/DarkTextViewDecorator.kt
@@ -0,0 +1,17 @@
+package id.assist.eathood.food.textviewdecorator
+
+import android.content.Context
+import android.widget.TextView
+import id.assist.eathood.contract.TextViewDecorator
+
+
+/**
+ * @author Putra Nugraha (putra.nugraha@dana.id)
+ * @version BrightViewDecorator, v 1.3.0 2019-09-20 08:40 by Putra Nugraha
+ */
+class DarkTextViewDecorator(val context: Context) : TextViewDecorator {
+
+    override fun doMagic(textView: TextView) {
+        textView.setTextColor(context.resources.getColor(android.R.color.black))
+    }
+}
