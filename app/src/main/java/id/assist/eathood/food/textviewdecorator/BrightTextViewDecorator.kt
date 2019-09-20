5a5d86f35d983d4c6fae4dccccd11f52651112ea
Putra <putra.nugraha@dana.id>
2019-09-20 09:15:43 +0700

    Add interface seggregation

--- app/src/main/java/id/assist/eathood/food/textviewdecorator/BrightTextViewDecorator.kt
+++ app/src/main/java/id/assist/eathood/food/textviewdecorator/BrightTextViewDecorator.kt
@@ -1,7 +1,9 @@
 package id.assist.eathood.food.textviewdecorator
 
 import android.content.Context
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import id.assist.eathood.contract.TextViewDecorator
 
 
@@ -9,9 +11,14 @@ import id.assist.eathood.contract.TextViewDecorator
  * @author Putra Nugraha (putra.nugraha@dana.id)
  * @version BrightViewDecorator, v 1.3.0 2019-09-20 08:40 by Putra Nugraha
  */
-class BrightTextViewDecorator(val context: Context) : TextViewDecorator {
+class BrightTextViewDecorator(val context: Context) : TextViewDecorator, View.OnClickListener {
+
+    override fun onClick(view: View?) {
+        Toast.makeText(context, "I am Red", Toast.LENGTH_SHORT).show()
+    }
 
     override fun doMagic(textView: TextView) {
         textView.setTextColor(context.resources.getColor(android.R.color.holo_red_light))
+        onClick(textView)
     }
 }
