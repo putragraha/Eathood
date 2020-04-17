f7b1e054573a9f14d8e860bb511502a6b57f12a7
putragraha <putra.graha.7@gmail.com>
2020-04-17 22:13:35 +0700

    Add LayoutPainter Implementation

---
+++ app/src/main/java/id/assist/eathood/food/viewpainter/layoutpainter/LightLayoutPainter.kt
@@ -0,0 +1,17 @@
+package id.assist.eathood.food.viewpainter.layoutpainter
+
+import android.content.Context
+import android.view.View
+import id.assist.eathood.contract.LayoutPainter
+
+
+/**
+ * @author Putra Nugraha (putra.nugraha@dana.id)
+ * @version LightLayoutPainter, v 1.3.0 2020-04-17 21:54 by Putra Nugraha
+ */
+class LightLayoutPainter(private val context: Context): LayoutPainter {
+
+    override fun doMagic(layout: View) {
+        layout.setBackgroundColor(context.resources.getColor(android.R.color.white))
+    }
+}
